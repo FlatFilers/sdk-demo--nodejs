b40ff2443f23db96ce0671e4178dd7b2409fe386
Max Blank <blank.maksym@gmail.com>
2021-08-25 11:48:13 -0500

    simple demo;

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const app = express();
+
+const EMBED_ID = 'YOUR_EMBED_ID'
+const PRIVATE_KEY = 'YOUR_PRIVATE_KEY'
+
+app.set('view engine', 'html');
+app.set('views', __dirname);
+app.engine('html', require('ejs').renderFile)
+
+app.get('/', async (_req, res) => {
+  const currentUser = {
+    email: 'max.blank@flatfile.io'
+  }
+
+  return res.render('index.html', {
+    token: await jwt.sign({
+      embed: EMBED_ID,
+      sub: currentUser.email
+    }, PRIVATE_KEY)
+  })
+})
+
+app.listen(4242)
